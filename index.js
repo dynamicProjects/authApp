309a8011b1aab06615796db322ba4815f038e001
shruti-source <shrutishrivastav77@gmail.com>
2024-04-08 16:24:11 -0500

    passport local authentication

--- index.js
+++ index.js
@@ -42,3 +42,5 @@ const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
 // PASPORT LOCAL AUTHENTICATION
 
 passport.use(UserDetails.createStrategy());
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
